892315c97882e5ef773b0ba2ca3beb3af19efd88
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2022-11-27 20:57:36 -0300

    Inicio projeto crud usando Nextjs

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import { Button } from "@chakra-ui/react";
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>TESTE NEXT JS</h1>
+      <Button colorScheme='blue'>Button</Button>
+    </div>
+  )
+}
